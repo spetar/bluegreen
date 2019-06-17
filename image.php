020003c98c75fddc673a7bbc0c92008eeb1bbf3e
spetar <sevaljevic.petar@gmail.com>
2019-06-17 15:17:47 +0200

    gr

--- image.php
+++ image.php
@@ -6,8 +6,9 @@ $white = imagecolorallocate($im, 255, 255, 255);
 $deployment = getenv("COLOR");
 
 if ( $deployment == 'blue') {
-  $color = imagecolorallocate($im, 0, 0, 255);
-// } elseif ($deployment == 'green')  {
+  $color = imagecolorallocate($im, 0, 255, 0);
+//  $color = imagecolorallocate($im, 0, 0, 255);
+ } elseif ($deployment == 'green')  {
   $color = imagecolorallocate($im, 0, 255, 0);
 } else {
   $color = imagecolorallocate($im, 0, 0, 255);
